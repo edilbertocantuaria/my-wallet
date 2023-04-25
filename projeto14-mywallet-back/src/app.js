35b8d1f1e9fe7fd00db3556bf751507e91a610ad
edilbertocantuaria <edilbertounbfga@gmail.com>
2023-04-25 12:44:39 -0300

    using cors

--- projeto14-mywallet-back/src/app.js
+++ projeto14-mywallet-back/src/app.js
@@ -1,16 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routers/routesIndex.js';
 import chalk from 'chalk';
 
-/*import dayjs from 'dayjs';
-import "dayjs/locale/pt-br.js";
-
-
-dayjs.locale('pt-br');*/
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors())
 app.use(router);
 
 
